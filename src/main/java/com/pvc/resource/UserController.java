4e939f26935f528a2ad129b6d6ef29ecb824c56c
Venkatesh <polneedi.venkatesh@gmail.com>
2019-06-24 00:34:00 +0530

    Post method added

--- src/main/java/com/pvc/resource/UserController.java
+++ src/main/java/com/pvc/resource/UserController.java
@@ -5,15 +5,14 @@ import com.pvc.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("rest")
 public class UserController {
-	
+
+	public static final String FAILURE = "Failure";
+	public static final String SUCCESS = "Success";
 	@Autowired
 	private IUserService userService;
 	
@@ -21,6 +20,20 @@ public class UserController {
 	public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
 		User user = userService.getUserById(id);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+
+	@PostMapping("user")
+	public ResponseEntity<String> createUser(@RequestBody User user) {
+		String response = null;
+		HttpStatus httpStatus = null;
+		if(user.getId() == 0){
+			response = FAILURE;
+			httpStatus = HttpStatus.BAD_REQUEST;
+		}else{
+			response = SUCCESS;
+			httpStatus = HttpStatus.OK;
+		}
+		return new ResponseEntity<String>(response, httpStatus);
 	}
 	
 
